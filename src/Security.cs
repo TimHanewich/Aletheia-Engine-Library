042a51dede2ae0e3b2d562f47eb602026357f857
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-01-03 21:38:06 -0500

    Replaced CompanyCik with company reference

--- src/Security.cs
+++ src/Security.cs
@@ -4,7 +4,7 @@ namespace Aletheia
 {
     public class Security
     {
-        public string CompanyCik {get; set;} //The CIK of the company this security is for
+        public Company Company {get; set;}
         public string Title {get; set;}
         public SecurityType SecurityType {get; set;}
     }
