43323eea608b299e70707ddd201e1790769dcc46
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-01-03 20:28:10 -0500

    Added Person

---
+++ src/Person.cs
@@ -0,0 +1,10 @@
+using System;
+
+namespace Aletheia
+{
+    public class Person
+    {
+        public string CIK {get; set;} //Central Index Key (Unique identifier from the SEC)
+        public string FullName {get; set;}
+    }
+}
