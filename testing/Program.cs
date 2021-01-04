3e42b24a0a5d2bd13e3cdef5abe71e2e0fb862fc
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-01-03 22:11:59 -0500

    Testing

---
+++ testing/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using SecuritiesExchangeCommission.Edgar;
+using Aletheia;
+using Newtonsoft.Json;
+
+namespace testing
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string content = System.IO.File.ReadAllText("C:\\Users\\tihanewi\\Downloads\\form4s\\amzn_long.xml");
+            AletheiaProcessor ap = new AletheiaProcessor();
+            SecurityTransaction[] transactions = ap.ProcessForm4(content, "999");
+            Console.WriteLine(JsonConvert.SerializeObject(transactions));
+        }
+    }
+}
