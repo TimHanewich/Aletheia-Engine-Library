614f0b4e0637c8abf68aa899f2ea97eaf4785151
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-01-04 23:03:26 -0500

    Testing

--- testing/Program.cs
+++ testing/Program.cs
@@ -2,6 +2,10 @@
 using SecuritiesExchangeCommission.Edgar;
 using Aletheia;
 using Newtonsoft.Json;
+using System.Net;
+using System.Net.Http;
+using System.Collections.Generic;
+using Aletheia.Cloud;
 
 namespace testing
 {
@@ -9,10 +13,29 @@ namespace testing
     {
         static void Main(string[] args)
         {
-            string content = System.IO.File.ReadAllText("C:\\Users\\tihanewi\\Downloads\\form4s\\amzn_long.xml");
-            AletheiaProcessor ap = new AletheiaProcessor();
-            SecurityTransaction[] transactions = ap.ProcessForm4(content, "999");
-            Console.WriteLine(JsonConvert.SerializeObject(transactions));
+            // List<SecurityTransaction> SecurityTransactions = new List<SecurityTransaction>();
+            // AletheiaProcessor ap = new AletheiaProcessor();
+            // HttpClient hc = new HttpClient();
+            // EdgarSearch es = EdgarSearch.CreateAsync(args[0], "4", null, EdgarSearchOwnershipFilter.only).Result;
+            // foreach (EdgarSearchResult esr in es.Results)
+            // {
+            //     if (esr.Filing == "4")
+            //     {
+            //         FilingDocument[] docs = esr.GetDocumentFormatFilesAsync().Result;
+            //         foreach (FilingDocument fd in docs)
+            //         {
+            //             if (fd.DocumentName.ToLower().Contains(".xml"))
+            //             {
+            //                 Console.WriteLine("Processing " + fd.Url + "...");
+            //                 HttpResponseMessage hrm = hc.GetAsync(fd.Url).Result;
+            //                 string content = hrm.Content.ReadAsStringAsync().Result;
+            //                 StatementOfChangesInBeneficialOwnership form4 = StatementOfChangesInBeneficialOwnership.ParseXml(content);
+            //                 SecurityTransaction[] trans = ap.ProcessForm4(content, esr.GetAccessionNumberAsync().Result);
+            //                 SecurityTransactions.AddRange(trans);
+            //             }
+            //         }
+            //     }
+            // }     
         }
     }
 }
