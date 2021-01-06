724e16f534ea8f60b17a021f596c68309fafb279
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-01-05 19:08:03 -0500

    Added derivative properties to security

--- src/Security.cs
+++ src/Security.cs
@@ -7,5 +7,12 @@ namespace Aletheia
         public Company Company {get; set;}
         public string Title {get; set;}
         public SecurityType SecurityType {get; set;}
+
+        //Derivative base (null if not derivative or not available)
+        public float? ConversionOrExcercisePrice {get; set;}
+        public DateTime? ExcercisableDate {get; set;}
+        public DateTime? ExpirationDate {get; set;}
+        public string UnderlyingSecurityTitle {get; set;}
+        public float UnderlyingSecurityQuantity {get; set;}
     }
 }
