ee6ea66fe73d9f445674a75c18a55d069c2b51eb
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-02-15 12:11:35 -0500

    Created held officer posiiton

---
+++ src/HeldOfficerPosition.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Aletheia
+{
+    public class HeldOfficerPosition
+    {
+        public Guid Id {get; set;}
+        public long Officer {get; set;}
+        public long Company {get; set;}
+        public string PositionTitle {get; set;}
+        public Guid ObservedOn {get; set;} //The filing that this officer title was observed on. (Guid relates to the record in SQL)
+    }
+}
