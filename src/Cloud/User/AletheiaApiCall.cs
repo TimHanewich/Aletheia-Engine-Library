717246cfdb9c37f8bd57fd6a81f0423ad3b1e395
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-03-02 20:43:32 -0500

    Made the consumed key property with an underscore which means related obj

--- src/Cloud/User/AletheiaApiCall.cs
+++ src/Cloud/User/AletheiaApiCall.cs
@@ -5,7 +5,7 @@ namespace Aletheia.Cloud.User
     public class AletheiaApiCall
     {
         public DateTime CalledAtUtc {get; set;}
-        public AletheiaApiKey ConsumedKey {get; set;}
+        public AletheiaApiKey _ConsumedKey {get; set;}
         public string Endpoint {get; set;}
         public ApiCallDirection Direction {get; set;}
     }
