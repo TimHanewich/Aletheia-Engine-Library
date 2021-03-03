5404ad8661ed302e3ba052de31af622b69f1038b
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-03-02 20:44:02 -0500

    Added ConsumedKey

--- src/Cloud/User/AletheiaApiCall.cs
+++ src/Cloud/User/AletheiaApiCall.cs
@@ -5,6 +5,7 @@ namespace Aletheia.Cloud.User
     public class AletheiaApiCall
     {
         public DateTime CalledAtUtc {get; set;}
+        public Guid ConsumedKey {get; set;}
         public AletheiaApiKey _ConsumedKey {get; set;}
         public string Endpoint {get; set;}
         public ApiCallDirection Direction {get; set;}
