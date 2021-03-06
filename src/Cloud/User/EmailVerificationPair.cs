a40103761d6e4e34bcdaab54d6826bda2d6f9718
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-03-05 22:17:07 -0500

    Added email verification code pair to code and ERD

---
+++ src/Cloud/User/EmailVerificationPair.cs
@@ -0,0 +1,23 @@
+using System;
+
+namespace Aletheia.Cloud.User
+{
+    public class EmailVerificationCodePair
+    {
+        public string Email {get; set;}
+        public string Code {get; set;}
+        public DateTime StartedAtUtc {get; set;}
+
+        public static EmailVerificationCodePair Create(string email)
+        {
+            EmailVerificationCodePair ToReturn = new EmailVerificationCodePair();
+
+            ToReturn.Email = email;
+            ToReturn.Code = Guid.NewGuid().ToString().Replace("-", "").Substring(0, 5).ToUpper();
+            ToReturn.StartedAtUtc = DateTime.UtcNow;
+
+            return ToReturn;
+        }
+
+    }
+}
