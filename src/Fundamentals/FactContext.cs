a6898be00deaaf98ad0e85d7d78f42fce6ac36db
TimHanewich <57418795+TimHanewich@users.noreply.github.com>
2021-03-14 21:37:50 -0400

    Made FromFiling Guid nullable

--- src/Fundamentals/FactContext.cs
+++ src/Fundamentals/FactContext.cs
@@ -5,7 +5,7 @@ namespace Aletheia.Fundamentals
     public class FactContext
     {
         public Guid Id {get; set;}
-        public Guid FromFiling {get; set;}
+        public Guid? FromFiling {get; set;}
         public SecFiling _FromFiling {get; set;}
         public DateTime? Start {get; set;}
         public DateTime End {get; set;}
