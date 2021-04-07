a722c63e1b8647e9ed0c304a4f0c3a019ee1be06
Tim Hanewich <tahanewich@live.com>
2021-04-06 21:23:20 -0400

    Removed newtonsoft.json from testing

--- testing/Program.cs
+++ testing/Program.cs
@@ -18,74 +18,7 @@ namespace testing
     {
         static void Main(string[] args)
         {
-            string drop_in = "C:\\Users\\tihanewi\\Downloads\\FULL_COLLECTS";
-            List<FundamentalsProcessingResult> Results = new List<FundamentalsProcessingResult>();
-            AletheiaProcessor ap = new AletheiaProcessor();
-
-            //<Make the dir if it doesnt exist
-            if (System.IO.Directory.Exists(drop_in) == false)
-            {
-                System.IO.Directory.CreateDirectory(drop_in);
-            }
             
-            string[] sp500 = TimHanewich.Investing.InvestingToolkit.GetEquityGroupAsync(EquityGroup.SP500).Result;
-            foreach (string s in sp500)
-            {
-                Console.WriteLine("Searching " + s.ToUpper() + "...");
-                EdgarSearch es = EdgarSearch.CreateAsync(s, "10-K").Result;
-                foreach (EdgarSearchResult esr in es.Results)
-                {
-                    Console.Write(s.ToUpper() + " --> " + esr.FilingDate.ToShortDateString());
-                    if (esr.Filing == "10-K")
-                    {
-                        Stream docstream = null;
-                        try
-                        {
-                            docstream = esr.DownloadXbrlDocumentAsync().Result;  
-                        }
-                        catch
-                        {
-                            Console.WriteLine("FAILURE while downloading stream.");
-                            continue;
-                        }
-
-                        XbrlInstanceDocument doc = null;
-                        try
-                        {
-                            doc = XbrlInstanceDocument.Create(docstream);  
-                        }
-                        catch
-                        {
-                            Console.WriteLine("FAILIURE while converting stream to instance document");
-                            continue;
-                        }
-
-                        FinancialStatement fs = null;
-                        try
-                        {
-                            fs = doc.CreateFinancialStatement();
-                        }
-                        catch
-                        {
-                            Console.WriteLine("FAILURE while converting to financial statement.");
-                            continue;
-                        }
-
-                        //Process!
-                        FundamentalsProcessingResult fpr = ap.ProcessFundamentals(fs);
-                        Results.Add(fpr);
-
-                        //Add to the drop in
-                        string ToDropIn = JsonConvert.SerializeObject(Results.ToArray());
-                        System.IO.File.WriteAllText(drop_in + "\\Total" + Results.Count.ToString() + ".json", ToDropIn);
-
-                        Console.WriteLine(" Successful!");
-
-                        
-                    }
-                }
-            }
-
         }
 
         public static void TryOne()
