d6c510cadcd272eb9d810c24b7df9ff01cb9b53c
Tim Hanewich <tahanewich@live.com>
2021-05-30 13:51:30 -0400

    Optional credential package constructor

--- src/Engine/Cloud/AletheiaCredentialPackage.cs
+++ src/Engine/Cloud/AletheiaCredentialPackage.cs
@@ -6,5 +6,16 @@ namespace Aletheia.Engine.Cloud
     {
         public string SqlConnectionString {get; set;}
         public string AzureStorageConnectionString {get; set;}
+        
+        public AletheiaCredentialPackage()
+        {
+            
+        }
+
+        public AletheiaCredentialPackage(string sql_connection_string, string az_storage_string)
+        {
+            SqlConnectionString = sql_connection_string;
+            AzureStorageConnectionString = az_storage_string;
+        }
     }
 }
