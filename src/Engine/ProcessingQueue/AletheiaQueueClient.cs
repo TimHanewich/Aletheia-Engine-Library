a3d48b75a3e7eaa8ac69f7f6c5f793281b3bd185
Tim Hanewich <tahanewich@live.com>
2021-06-06 13:05:31 -0400

    Set up table initialization

--- src/Engine/ProcessingQueue/AletheiaQueueClient.cs
+++ src/Engine/ProcessingQueue/AletheiaQueueClient.cs
@@ -1,6 +1,8 @@
 using System;
 using System.Data.SqlClient;
 using System.IO;
+using System.Threading.Tasks;
+using System.Collections.Generic;
 
 namespace Aletheia.Engine.ProcessingQueue
 {
@@ -14,6 +16,53 @@ namespace Aletheia.Engine.ProcessingQueue
             SqlConnectionString = sql_connection_string;
         }
 
+        public async Task InitializeTablesAsync()
+        {
+            //Open the SQL connection
+            SqlConnection sqlcon = GetSqlConnection();
+            sqlcon.Open();
+
+            //Get a list of all the tables in the DB right now
+            SqlCommand sqlcmdtns = new SqlCommand("select TABLE_NAME from information_schema.tables", sqlcon);
+            SqlDataReader dr = await sqlcmdtns.ExecuteReaderAsync();
+            List<string> ExistingTableNames = new List<string>();
+            while (dr.Read())
+            {
+                if (dr.IsDBNull(0) == false)
+                {
+                    ExistingTableNames.Add(dr.GetString(0));
+                }
+            }
+            dr.Close();
+
+            //ProcessingConfiguration
+            if (ExistingTableNames.Contains("ProcessingConfiguration") == false)
+            {
+                string cmd = "create table ProcessingConfiguration (Id tinyint primary key not null, InternalProcessingPaused bit, ResumeInternalProcessingInSeconds smallint)";
+                await ExecuteNonQueryAsync(cmd);
+            }
+
+            //ProcessingTask
+            if (ExistingTableNames.Contains("ProcessingTask") == false)
+            {
+                string cmd = "create table ProcessingTask (Id uniqueidentifier primary key not null, AddedAtUtc datetime, TaskType tinyint, PriorityLevel smallint)";
+                await ExecuteNonQueryAsync(cmd);
+            }
+
+            //SecFilingTaskDetails
+            if (ExistingTableNames.Contains("SecFilingTaskDetails") == false)
+            {
+                string cmd = "create table SecFilingTaskDetails (Id uniqueidentifier primary key not null, ParentTask uniqueidentifier, FilingUrl varchar(255))";
+                await ExecuteNonQueryAsync(cmd);
+            }
+
+        }
+
+        // public async Task<ProcessingConfiguration> GetProcessingConfigurationAsync(byte id)
+        // {
+        //     string cmd = "select InternalProcessingPaused, ResumeInternalProcessingInSeconds"
+        // }
+
         private SqlConnection GetSqlConnection()
         {
             if (SqlConnectionString == null)
@@ -26,6 +75,15 @@ namespace Aletheia.Engine.ProcessingQueue
             }
             SqlConnection sqlcon = new SqlConnection(SqlConnectionString);
             return sqlcon;
+        }
+
+        private async Task ExecuteNonQueryAsync(string cmd)
+        {
+            SqlConnection sqlcon = GetSqlConnection();
+            sqlcon.Open();
+            SqlCommand sqlcmd = new SqlCommand(cmd, sqlcon);
+            await sqlcmd.ExecuteNonQueryAsync();
+            await sqlcon.CloseAsync();
         }
 
     }
