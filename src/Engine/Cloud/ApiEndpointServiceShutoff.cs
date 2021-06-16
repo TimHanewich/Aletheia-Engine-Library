9ece0a10ac6de8b7f7b6f96ab9693dd21b21cadc
Tim Hanewich <tahanewich@live.com>
2021-06-16 07:53:33 -0400

    Api service shutoff classes

---
+++ src/Engine/Cloud/ApiEndpointServiceShutoff.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aletheia.Engine.Cloud
+{
+    //This is used to shut off a specific endpoint
+    public class ApiEndpointServiceShutoff
+    {
+        public string Endpoint {get; set;} //The endpoint title (ID that is logged in the ApiCall)
+        public string DenialMessage {get; set;} //The message that should be returned to the user when they attempt to call this endpoint but will be denied.
+    }
+}
---
+++ src/Engine/Cloud/ApiServiceShutoff.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aletheia.Engine.Cloud
+{
+    public class ApiServiceShutoff
+    {
+        public bool Shutoff {get; set;}
+        public string DenialMessage {get; set;} //This message will be returned to the users trying to use the API service when it is denying them.
+        public ApiEndpointServiceShutoff[] ShutoffEndpoints {get; set;} //Instead of shutoff the entire service, you can also block specific endpoints.
+    }
+}
