a6c4eee814c14b14c67c32afacb3df269f0d9236
Tim Hanewich <tahanewich@live.com>
2021-06-19 10:14:03 -0400

    Added AletheiaEndpoint enum

---
+++ src/Engine/Cloud/User/AletheiaEndpoint.cs
@@ -0,0 +1,22 @@
+using System;
+
+namespace Aletheia.Engine.Cloud.User
+{
+    public enum AletheiaEndpoint
+    {
+        AffiliatedOwners = 0,
+        Consumption = 1,
+        EntityFilings = 2,
+        FinancialFactTrend = 3,
+        Financials = 4,
+        LatestTransactions = 5,
+        MyCalls = 6,
+        NewFilings = 7,
+        OpenCommonFinancials = 8,
+        OpenForm4 = 9,
+        SearchEntities = 10,
+        StockData = 11,
+        SubscribeToNewFilingsWebhook = 12,
+        UnsubscribeFromNewFilingsWebhookById = 13
+    }
+}
