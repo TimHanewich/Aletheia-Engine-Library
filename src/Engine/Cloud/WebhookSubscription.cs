da59ba3235c694489184fa8f9546c463bd06ff80
Tim Hanewich <tahanewich@live.com>
2021-06-21 22:32:44 -0400

    Added Id to Webhook subscription

--- src/Engine/Cloud/WebhookSubscription.cs
+++ src/Engine/Cloud/WebhookSubscription.cs
@@ -4,6 +4,7 @@ namespace Aletheia.Engine.Cloud
 {
     public class WebhookSubscription
     {
+        public Guid Id {get; set;}
         public string Endpoint {get; set;}
         public DateTime AddedAtUtc {get; set;}
         public Guid RegisteredToKey {get; set;}
