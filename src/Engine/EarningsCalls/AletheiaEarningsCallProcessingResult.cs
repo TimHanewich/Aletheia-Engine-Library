6bf05bc722d14bffeb2bf1d226c66567c15036a7
Tim Hanewich <tahanewich@live.com>
2021-07-05 08:32:06 -0400

    Added methods for finding call participants and spoken remarks

--- src/Engine/EarningsCalls/AletheiaEarningsCallProcessingResult.cs
+++ src/Engine/EarningsCalls/AletheiaEarningsCallProcessingResult.cs
@@ -9,5 +9,29 @@ namespace Aletheia.Engine.EarningsCalls
         public SpokenRemark[] SpokenRemarks {get; set;}
         public CallParticipant[] CallParticipants {get; set;}
         public SpokenRemarkHighlight[] SpokenRemarkHighlights {get; set;}
+
+        public SpokenRemark FindSpokenRemark(Guid id)
+        {
+            foreach (SpokenRemark sr in SpokenRemarks)
+            {
+                if (sr.Id == id)
+                {
+                    return sr;
+                }
+            }
+            throw new Exception("Unable to find SpokenRemark with Id '" + id.ToString() + "'");
+        }
+
+        public CallParticipant FindCallParticipant(Guid id)
+        {
+            foreach (CallParticipant cp in CallParticipants)
+            {
+                if (cp.Id == id)
+                {
+                    return cp;
+                }
+            }
+            throw new Exception("Unable to find CallParticipant with Id '" + id.ToString() + "'");
+        }
     }
 }
