492a5bfa04056a6a0874a875db17c48807c094b4
Tim Hanewich <tahanewich@live.com>
2021-07-04 21:30:36 -0400

    Made remark highlight constructor

--- src/Engine/EarningsCalls/ProcessingComponents.cs/RemarkKeyword.cs
+++ src/Engine/EarningsCalls/ProcessingComponents.cs/RemarkKeyword.cs
@@ -7,5 +7,17 @@ namespace Aletheia.Engine.EarningsCalls.ProcessingComponents
         public string KeywordPhrase {get; set;}
         public HighlightCategory Category {get; set;}
         public float RatingWeight {get; set;}
+
+        public RemarkKeyword()
+        {
+
+        }
+
+        public RemarkKeyword(string phrase, HighlightCategory category, float weight)
+        {
+            KeywordPhrase = phrase;
+            Category = category;
+            RatingWeight = weight;
+        }
     }
 }
