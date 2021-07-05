b04d704a35f5977277afa9411453b01ff020cef1
Tim Hanewich <tahanewich@live.com>
2021-07-04 21:07:39 -0400

    Added SequenceNumber to spoken remark class

--- src/Engine/EarningsCalls/SpokenRemark.cs
+++ src/Engine/EarningsCalls/SpokenRemark.cs
@@ -8,5 +8,9 @@ namespace Aletheia.Engine.EarningsCalls
         public Guid FromCall {get; set;}
         public Guid SpokenBy {get; set;}
         public Guid BlobId {get; set;}
+        public int SequenceNumber {get; set;}
+
+        //Not actually part of the SQL table, but here for convenience in C#
+        public string Remark {get; set;}
     }
 }
