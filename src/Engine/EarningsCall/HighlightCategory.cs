bcb8ca905de52c48d52f45789fa4324d358418b3
Tim Hanewich <tahanewich@live.com>
2021-07-04 20:12:39 -0400

    AddedSpokenRemarkHighlight

---
+++ src/Engine/EarningsCall/HighlightCategory.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Aletheia.Engine.EarningsCall
+{
+    public enum HighlightCategory
+    {
+        Revenue = 0,
+        Earnings = 1,
+        CashFlow = 2
+    }
+}
---
+++ src/Engine/EarningsCall/SpokenRemarkHighlight.cs
@@ -0,0 +1,13 @@
+using System;
+
+namespace Aletheia.Engine.EarningsCall
+{
+    public class SpokenRemarkHighlight
+    {
+        public Guid Id {get; set;}
+        public Guid SubjectRemark {get; set;}
+        public int BeginPosition {get; set;}
+        public int EndPosition {get; set;}
+        public HighlightCategory Category {get; set;}
+    }
+}
